a260798e2379c5d41a6f27cee821052aa0e8cdca
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-06-26 18:58:41 +0900

    Variables and lists commit

--- hello.py
+++ hello.py
@@ -2,4 +2,57 @@
 
 print 'Content-type: text/html'
 print ''
-print 'Hello Python!'
+# print 'Hello Python!'
+
+age = 35
+
+print age
+
+print ''
+
+pi = 3.14
+
+print pi
+
+name = "Rob"
+
+print name
+
+print age/pi
+
+number = "5"
+
+print number * age
+
+print '<br>'
+
+print int(number) * 5
+
+print int(number) * age
+
+print int(number) * pi
+
+str = "My name is Rob"
+
+print str[0]
+print str[0:6]
+print str[5]
+
+mylist = ["Apple", "banana", "orange"]
+print mylist
+print mylist[1]
+print mylist[1:4]
+
+myTuple = (1,2,3,4)
+print myTuple
+
+dict = {}
+dict["dad"] = "Rob"
+dict["mum"] ="Kirsten"
+dict[1] = "Tommy"
+dict[2] = "Raphie"
+
+print dict
+print dict["mum"]
+print dict.keys()
+print dict.values()
