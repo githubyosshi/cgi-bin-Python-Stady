cac53295010aef06b63e56ad9a1255dcc277ed67
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-06-28 18:13:31 +0900

    If commit

--- hello.py
+++ hello.py
@@ -3,33 +3,9 @@
 print 'Content-type: text/html'
 print ''
 
-# for i in range(11):
-for i in range(5,11):
-  print i
-  print "Rob"
+name = "Kirsten"
 
-print "Sam"
-
-list = ["pizza","chocholate","ice cream"]
-
-print "<br>"
-for food in list:
-  print "I like eating " + food + "."
-  print "<br>"
-
-
-x = 0
-while x <= 10:
-  print x
-  x = x + 1
-  # x += 1
-print "<br>"
-
-ages = {}
-ages["Rob"] = 35
-ages["Kirsten"] = 36
-ages["Tommy"] = 5
-ages["Rophie"] = 1
-
-for age in ages:
- print age + " is " + str(ages[age]) + "."
+if name == "Rob" or name == "Kirsten" :
+  print "Hello " + name + "!"
+else:
+  print "I don't know."
